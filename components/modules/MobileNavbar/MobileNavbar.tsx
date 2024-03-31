7fd81a4a8881404a8091e7ee140b7bee4a03c7e3
Eva <lilkittys@mail.ru>
2024-04-01 01:57:23 +0400

    Button more
    Update: Button more

--- components/modules/MobileNavbar/MobileNavbar.tsx
+++ components/modules/MobileNavbar/MobileNavbar.tsx
@@ -1,16 +1,28 @@
 'use client'
-import { useLang } from "@/hooks/useLang"
 import Link from "next/link";
+import { closeCatalogMenu, closeMenu, openCatalogMenu, openMenu } from "@/context/madals";
+import { useLang } from "@/hooks/useLang"
+import { addOverFlowHiddenFromBody } from "@/lib/utils/common";
 
 
 const MobileNavbar = () => {
     const { lang, translations } = useLang();
+    const handlOpenMenu = () => {
+        addOverFlowHiddenFromBody()
+        openMenu()
+        closeCatalogMenu();
+    }
+    const handlOpenCatalogMenu = () => {
+        addOverFlowHiddenFromBody()
+        openCatalogMenu()
+        closeMenu()
+    }
   return (
     <div className="mobile-navbar">
       <Link href = '/' className="mobile-navbar__btn">
         {translations[lang].breadcrumbs.main}
       </Link>
-      <button className="btn-reset mobile-navbar__btn">
+      <button className="btn-reset mobile-navbar__btn" onClick={handlOpenCatalogMenu}>
       {translations[lang].breadcrumbs.catalog}
       </button>
       <Link className="mobile-navbar__btn" href="/favorites">
@@ -19,7 +31,7 @@ const MobileNavbar = () => {
       <Link className="mobile-navbar__btn" href="/cart">
         {translations[lang].breadcrumbs.cart}
       </Link>
-      <button className="btn-reset mobile-navbar__btn">
+      <button className="btn-reset mobile-navbar__btn" onClick={handlOpenMenu}>
         {translations[lang].common.more}
       </button>
     </div>
--- context/madals.ts
+++ context/madals.ts
@@ -4,8 +4,15 @@ const modals = createDomain();
 
 export const openMenu = modals.createEvent();
 export const closeMenu = modals.createEvent();
+export const openCatalogMenu = modals.createEvent();
+export const closeCatalogMenu = modals.createEvent();
 
 export const $menuIsOpen = modals
     .createStore(false)
     .on(openMenu, () => true)
     .on(closeMenu, () => false)
+
+export const $catalogMenuIsOpen = modals
+    .createStore(false)
+    .on(openCatalogMenu, () => true)
+    .on(closeCatalogMenu, () => false)
