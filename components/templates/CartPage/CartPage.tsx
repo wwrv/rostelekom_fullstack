bd40c6e478bdfd7cd791ee7b1999bb64a7225a0a
Eva <lilkittys@mail.ru>
2024-06-29 17:03:45 +0300

    Added
    Products are displayed on the cart page

--- components/templates/CartPage/CartPage.tsx
+++ components/templates/CartPage/CartPage.tsx
@@ -1,15 +1,23 @@
 'use client'
+import { getCartItemsFx } from '@/api/cart'
 import HeadingWithCount              from '@/components/elements/HeadingWithCount/HeadingWithCount'
 import Breadcrumbs                   from '@/components/modules/Breadcrumbs/Breadcrumbs'
+import CartList from '@/components/modules/CartPage/CartList'
+import { basePropsForMotion } from '@/constants/motion'
 import { useBreadcrumbs }            from '@/hooks/useBreadcrumbs'
 import { useCartByAuth }             from '@/hooks/useCartByAuth'
 import { useLang }                   from '@/hooks/useLang'
 import { countWholeCartItemsAmount } from '@/lib/utils/cart'
+import { isUserAuth } from '@/lib/utils/common'
 import styles                        from '@/styles/cart-page/index.module.scss'
+import cartSkeletonStyles            from '@/styles/cart-skeleton/index.module.scss'
+import { useUnit }                   from 'effector-react'
+import { motion } from 'framer-motion'
 
 
 
 const CartPage = () => {
+  const cartSpinner                                   = useUnit(getCartItemsFx.pending)
   const currentCartByAuth                             = useCartByAuth()
   const { lang, translations }                        = useLang()
   const { getDefaultTextGenerator, getTextGenerator } = useBreadcrumbs('cart')
@@ -27,6 +35,38 @@ const CartPage = () => {
                 count={countWholeCartItemsAmount(currentCartByAuth)}
                 title={translations[lang].breadcrumbs.cart}
               />
+              <div className={styles.cart__inner}>
+                <div className={styles.cart__left}>
+                  {cartSpinner && (
+                    <motion.ul
+                      {...basePropsForMotion}
+                      className = {cartSkeletonStyles.sleleton}
+                    >
+                      {Array.from(new Array(3)).map((_, i) => (
+                        <li
+                          key={i}
+                          className={cartSkeletonStyles.skeleton__item}
+                        >
+                          <div 
+                            className={cartSkeletonStyles.skeleton__item__light}
+                          />
+                        </li>
+                      ))}
+                    </motion.ul>
+                  )}
+                  {!cartSpinner && (
+                    <motion.ul
+                      {...basePropsForMotion}
+                      className={`list-reset ${styles.cart__list}`}
+                    >
+                      <CartList />
+                    </motion.ul>
+                  )}
+                </div>
+                <div className={styles.cart__right}>
+
+                </div>
+              </div>
             </div>
         </section>
     </main>
